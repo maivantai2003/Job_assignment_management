89d8db5301d724177debbc9bcc35c6a1a7a64cf3
Van Tai <vantai08122003@gmail.com>
2024-09-23 22:00:44 +0700

    Add Policy CORS

--- Job_assignment_management.Api/Program.cs
+++ Job_assignment_management.Api/Program.cs
@@ -6,7 +6,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("ApiPolicy", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,7 +23,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors();
 app.UseAuthorization();
 
 app.MapControllers();
