4ac1afd8dc5680b07cd69e61f4db19c14d22fcc7
Van Tai <vantai08122003@gmail.com>
2024-11-21 13:31:32 +0700

    update

--- Job_assignment_management.Api/Program.cs
+++ Job_assignment_management.Api/Program.cs
@@ -124,7 +124,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseCors(policy => policy
-    .WithOrigins("https://job-assignment-manager-deploy-azure-6n9626tsk-vantais-projects.vercel.app")
+    .WithOrigins("https://job-assignment-manager-deploy-azure.vercel.app")
     .AllowAnyHeader()
     .AllowAnyMethod()
     .AllowCredentials());
