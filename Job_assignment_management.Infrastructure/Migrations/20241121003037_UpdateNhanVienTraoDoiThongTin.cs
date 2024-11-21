5554df0bf2f1e2cf728ba02ab472a68e17436f68
Van Tai <vantai08122003@gmail.com>
2024-11-21 07:31:34 +0700

    update NhanVienTraoDoiThongTin

---
+++ Job_assignment_management.Infrastructure/Migrations/20241121003037_UpdateNhanVienTraoDoiThongTin.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Job_assignment_management.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateNhanVienTraoDoiThongTin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
